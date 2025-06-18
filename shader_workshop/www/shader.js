ubitux/ShaderWorkshop

commit 6e03f5ebcc11460097462977786e56847aa8af39

js: notify render loop of cancellation to prevent a race condition

shader_workshop/www/shader.js
@@ -39,6 +39,9 @@ class CanvasShader {
     if (this.animationFrame !== null) {
       cancelAnimationFrame(this.animationFrame);
       this.animationFrame = null;
+      const p = this.prog;
+      this.prog = null; // used to notify render loop
+      gl.deleteProgram(p);
     }
 
     // A big triangle cropped by the viewport to fake a quad geometry (because it's faster)
@@ -54,8 +57,6 @@ class CanvasShader {
     const vs = this.compileShader(vsSrc, gl.VERTEX_SHADER);
     const fs = this.compileShader(fsSrc, gl.FRAGMENT_SHADER);
 
-    if (this.prog !== null)
-      gl.deleteProgram(this.prog);
     const prog = gl.createProgram();
     this.prog = prog;
     gl.attachShader(prog, vs);
@@ -88,6 +89,11 @@ class CanvasShader {
       } else if (action == Action.Resume) {
         action = null;
         startTime += time - pauseTime;
+      }
+
+      if (!this.prog) {
+        console.log("render loop canceled");
+        return;
       }
 
       if (paused) time = pauseTime;

commit 6a8a619849ea2ae5371b6b33728c4cd80972cdc9

js: fix access to gl
Regression introduced in 6e03f5ebcc11460097462977786e56847aa8af39.

shader_workshop/www/shader.js
@@ -36,6 +36,8 @@ class CanvasShader {
   }
 
   async loadFragment(frag) {
+    const gl = this.gl;
+
     if (this.animationFrame !== null) {
       cancelAnimationFrame(this.animationFrame);
       this.animationFrame = null;
@@ -53,7 +55,6 @@ class CanvasShader {
 
     const fsSrc = await fetch(frag, {cache: 'no-store'}).then(r => r.text());
 
-    const gl = this.gl;
     const vs = this.compileShader(vsSrc, gl.VERTEX_SHADER);
     const fs = this.compileShader(fsSrc, gl.FRAGMENT_SHADER);
 